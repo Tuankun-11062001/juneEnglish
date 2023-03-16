db27c6258bf6365b882f55320c51311514290a42
I-TuanNguyen <i-tuannguyen@titancorp.vn>
2023-03-16 21:01:32 +0700

    create success component Loading...

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,5 +1,15 @@
+import { Box, Container, Flex, Img, Text } from "@chakra-ui/react";
 import React from "react";
-
+import loadingSvg from "../../assets/cube_loading.svg";
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <Container bg="white" pos="fixed" top={0} left={0} right={0} bottom={0}>
+      <Flex justify="center" alignItems="center" h="100%" flexDir="column">
+        <Img src={loadingSvg} alt="loading" />
+        <Text mt="2rem" fontSize="1.3rem" color="blackAlpha.400">
+          Đang tải dữ liệu các bạn đợi xíu nha....
+        </Text>
+      </Flex>
+    </Container>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Loading from "../../components/loading/Loading";
 
 export default function Home() {
   return <div>Home</div>;
