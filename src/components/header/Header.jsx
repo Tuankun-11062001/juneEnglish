cb8cf4e7b1482fa7c2f3f49a206d6c7aaebb9f1d
I-TuanNguyen <i-tuannguyen@titancorp.vn>
2023-03-16 21:53:44 +0700

    Done 80% header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,39 @@
+import { Button, Flex, Spacer, Text, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { BiMenuAltRight } from "react-icons/bi";
+import HeaderDrawer from "../header_drawer/HeaderDrawer";
 
 export default function Header() {
+  //   const {
+  //     isOpen: isOpenDrawer,
+  //     onClose: onCloseDrawer,
+  //     onOpen: onOpenDrawer,
+  //   } = useDisclosure();
+
+  const { isOpen = false, onOpen, onClose } = useDisclosure();
+
   return (
-    <div>
-      Header
+    <>
+      <Flex bg="blue.600" padding="1rem" alignItems="center" color="white">
+        <Text fontSize="1.5rem" fontWeight="500">
+          June'English
+        </Text>
+        <Spacer />
+        <Button
+          variant="solid"
+          border="none"
+          bg="transparent"
+          onClick={onOpen}
+          _hover={{ bg: "blue.200", color: "white" }}
+        >
+          <BiMenuAltRight
+            style={{ width: "2.3rem", height: "2.3rem", color: "white" }}
+          />
+        </Button>
+      </Flex>
+      <HeaderDrawer open={isOpen} close={onClose} />
       <Outlet />
-    </div>
+    </>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Loading from "../../components/loading/Loading";
 
 export default function Home() {
   return <div>Home</div>;
