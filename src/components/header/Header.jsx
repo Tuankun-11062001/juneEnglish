9fe55f74c31b5b20c1f0419e5ba8ee4c9f631876
I-TuanNguyen <i-tuannguyen@titancorp.vn>
2023-03-18 10:11:57 +0700

    change absolute => fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,46 +8,44 @@ export default function Header() {
   return (
     <Container maxW="full">
       <Outlet />
-      <Container position="relative" h="100vh">
-        <Flex
-          pos="absolute"
-          bottom="5rem"
-          left="50%"
-          transform="translateX(-50%)"
-          padding="1rem 3rem"
-          bg="white"
-          boxShadow="2xl"
-          border="1px"
-          borderColor="gray.300"
-          borderRadius="1rem"
-          gap="3rem"
-          alignItems="center"
-          bgGradient="linear(to-r,blue.300, pink.300)"
-        >
-          <Tooltip label="Home" aria-label="A tooltip">
-            <NavLink to="/" className="header_link">
-              <BiHomeAlt2 />
-            </NavLink>
-          </Tooltip>
+      <Flex
+        pos="fixed"
+        bottom="3rem"
+        left="50%"
+        transform="translateX(-50%)"
+        padding="1rem 3rem"
+        bg="white"
+        boxShadow="2xl"
+        border="1px"
+        borderColor="gray.300"
+        borderRadius="1rem"
+        gap="3rem"
+        alignItems="center"
+        bgGradient="linear(to-r,blue.300, pink.300)"
+      >
+        <Tooltip label="Home" aria-label="A tooltip">
+          <NavLink to="/" className="header_link">
+            <BiHomeAlt2 />
+          </NavLink>
+        </Tooltip>
 
-          <Tooltip label="Add One">
-            <NavLink to="add-task" className="header_link">
-              <BiPlusCircle />
-            </NavLink>
-          </Tooltip>
+        <Tooltip label="Add One">
+          <NavLink to="add-task" className="header_link">
+            <BiPlusCircle />
+          </NavLink>
+        </Tooltip>
 
-          <Tooltip label="Manager task">
-            <NavLink to="manager-task" className="header_link">
-              <BiTask />
-            </NavLink>
-          </Tooltip>
-          <Tooltip label="Practice">
-            <NavLink to="practice" className="header_link">
-              <BiBrain />
-            </NavLink>
-          </Tooltip>
-        </Flex>
-      </Container>
+        <Tooltip label="Manager task">
+          <NavLink to="manager-task" className="header_link">
+            <BiTask />
+          </NavLink>
+        </Tooltip>
+        <Tooltip label="Practice">
+          <NavLink to="practice" className="header_link">
+            <BiBrain />
+          </NavLink>
+        </Tooltip>
+      </Flex>
     </Container>
   );
 }
